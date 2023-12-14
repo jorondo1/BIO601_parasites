2c6ba43105d9eacc9961b14df52921af2e5b8c23
Jonathan Rondeau-Leclaire <97983060+jorondo1@users.noreply.github.com>
2023-12-13 19:35:59 -0500

    Delete draft_script.R

--- draft_script.R
+++
@@ -1,5 +0,0 @@
-library(pacman); p_load(magrittr)
-spe_table <- readRDS("projet_BIO601/brainstorm/plant_data/spe_table.rds")
-taxa_table <-  readRDS("projet_BIO601/brainstorm/plant_data/RDP_taxa.rds")
-
-spe_table %>% rownames
